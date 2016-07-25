83464ea61bb146f42b1e03f861196ba29cc4f795
Justin Morgan <jmorgan@overlake.org>
2016-07-25 00:07:15 -0700

    modify message after order submission to include name and quantity add checkboxes for whipped cream and chocolate with result reported in order message add scrolling

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.android.justjava;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 /**
  * This app displays an order form to order coffee.
  */
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     int quantity = 2;
 
@@ -22,9 +23,11 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = quantity * 5;
-        String priceMessage = "Total: $" + price + "\nThank you!";
-        displayPrice(priceMessage);
+        int price = calculatePrice();
+        boolean hasWhippedCream = ((CheckBox)findViewById(R.id.whipped_cream_checkbox)).isChecked();
+        boolean hasChocolate = ((CheckBox)findViewById(R.id.chocolate_checkbox)).isChecked();
+        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate);
+        displayMessage(priceMessage);
     }
 
     public void increment(View view) {
@@ -38,6 +41,24 @@ public class MainActivity extends ActionBarActivity {
     }
 
     /**
+     * Calculate the total price of the coffee order
+     * @return
+     */
+    private int calculatePrice() {
+        int price = quantity * 5;
+        return price;
+    }
+
+    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate) {
+        //CheckBox whippedCreamCheck = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        String message = "Name: Jmo";
+        message += "\nAdd whipped cream? " + addWhippedCream;
+        message += "\nAdd chocolate? " + addChocolate;
+        message += "\nQuantity: " + quantity + "\nTotal: $" + price + "\nThank you!";
+        return message;
+    }
+
+    /**
      * This method displays the given quantity value on the screen.
      */
     private void displayQuantity(int number) {
@@ -48,8 +69,8 @@ public class MainActivity extends ActionBarActivity {
     /**
      * This method displays the given text on the screen.
      */
-    private void displayPrice(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+    private void displayMessage(String message) {
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 }
